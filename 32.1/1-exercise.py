a4a0a294daa898d8141a3449dceea3cf09001e9b
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-01-17 15:18:18 -0300

    Add: 1

---
+++ 32.1/1-exercise.py
@@ -0,0 +1,10 @@
+# Exercício 1: Crie uma função que receba dois números e retorne o maior deles.
+
+def returnTheBiggestNumber(num1, num2):
+  if num1 > num2:
+    return num1
+  else:
+    return num2
+
+
+print(returnTheBiggestNumber(52, 50))
